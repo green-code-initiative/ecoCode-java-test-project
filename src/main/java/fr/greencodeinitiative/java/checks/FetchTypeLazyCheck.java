d58c725aec84ad48695ba38898292829397eb29a
mohamed qelai <mohamed.qelai@gmail.com>
2023-04-06 11:38:11 +0200

    add CRMJVM205

---
+++ src/main/java/fr/greencodeinitiative/java/checks/FetchTypeLazyCheck.java
@@ -0,0 +1,24 @@
+package fr.greencodeinitiative.java.checks;
+
+
+
+import jakarta.persistence.*;
+import org.hibernate.annotations.Table;
+
+import java.util.regex.Pattern;
+@Entity
+@Table(name = "ECO_CODE")
+class FetchTypeLazyCheck {
+    FetchTypeLazyCheck(FetchTypeLazyCheck mc) {
+    }
+
+    @Id
+    @GeneratedValue
+    @Column(name="ORDER_ID")
+    private Long orderId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name="USER_ID")
+    private Object user;
+   
+}

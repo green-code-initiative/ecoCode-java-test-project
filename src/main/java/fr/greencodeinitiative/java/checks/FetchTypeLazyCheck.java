754edfb308fecdfbfdce07d29410943cf55c5986
mohamed qelai <mohamed.qelai@gmail.com>
2023-04-06 11:51:41 +0200

    format code

--- src/main/java/fr/greencodeinitiative/java/checks/FetchTypeLazyCheck.java
+++ src/main/java/fr/greencodeinitiative/java/checks/FetchTypeLazyCheck.java
@@ -1,24 +1,18 @@
 package fr.greencodeinitiative.java.checks;
 
 
-
 import fr.greencodeinitiative.java.utils.User;
 import jakarta.persistence.*;
-import org.hibernate.annotations.Table;
 
-import java.util.regex.Pattern;
 @Entity
 class FetchTypeLazyCheck {
-    FetchTypeLazyCheck(FetchTypeLazyCheck mc) {
-    }
-
     @Id
     @GeneratedValue
-    @Column(name="ORDER_ID")
+    @Column(name = "ORDER_ID")
     private Long orderId;
 
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name="USER_ID")
+    @JoinColumn(name = "USER_ID")
     private User user;
-   
+
 }
